39194cade9f5047449670212be076f0f53e3c8a1
ChiracOlivier <chiracolivier28@gmail.com>
2020-03-13 00:06:58 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// import * as firebase from 'firebase';
+// import {TranslateService} from 'ng2-translate';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stagen8';
+  constructor() { // translate: TranslateService) {
+    /* translate.addLangs(['en', 'fr']);
+
+     // this language will be used as a fallback when a translation isn't found in the current language
+     translate.setDefaultLang('en');
+
+     // the lang to use, if the lang isn't available, it will use the current loader to get them
+     const browserLang = translate.getBrowserLang();
+     translate.use(browserLang.match(/en|fr/) ? browserLang : 'en');*/
+
+    const firebaseConfig = {
+      apiKey: 'AIzaSyBqIBGZr9N0iTIQihIXWqEzu0EmGAyZ-9Y',
+      authDomain: 'flore1-380dc.firebaseapp.com',
+      databaseURL: 'https://flore1-380dc.firebaseio.com',
+      projectId: 'flore1-380dc',
+      storageBucket: 'flore1-380dc.appspot.com',
+      messagingSenderId: '379605375077',
+      appId: '1:379605375077:web:e68e1be4856b8e862c8e8c',
+      measurementId: 'G-85QHX8FZPE'
+    };
+    //  firebase.initializeApp(firebaseConfig);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+// import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NouscontacterComponent } from './nouscontacter/nouscontacter.component';
+import { AproposComponent } from './apropos/apropos.component';
+// import {Http, HttpModule} from '@angular/http';
+import { ErrorpageComponent } from './errorpage/errorpage.component';
+import { HomeComponent } from './home/home.component';
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
