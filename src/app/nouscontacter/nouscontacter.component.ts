d477c31323d8aae42d3a18186d6fd0a3cbcfeead
ChiracOlivier <chiracolivier28@gmail.com>
2020-03-13 00:22:24 +0100

    initial commit

---
+++ src/app/nouscontacter/nouscontacter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nouscontacter',
+  templateUrl: './nouscontacter.component.html',
+  styleUrls: ['./nouscontacter.component.scss']
+})
+export class NouscontacterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
