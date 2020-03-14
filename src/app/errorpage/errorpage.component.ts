b9b3c9329d9917cadb773d3f3cb2fd227615aacb
ChiracOlivier <chiracolivier28@gmail.com>
2020-03-14 16:39:22 +0100

    add error page

--- src/app/errorpage/errorpage.component.ts
+++ src/app/errorpage/errorpage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-errorpage',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ErrorpageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
-
+  goback() {
+    this.router.navigate(['home']);
+  }
 }
